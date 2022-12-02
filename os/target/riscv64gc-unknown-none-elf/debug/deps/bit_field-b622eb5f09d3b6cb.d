10180f6378167ba77747d891c1eab2b724f29366
Specialwu-cpu <1307053712@qq.com>
2022-12-02 20:04:40 +0800

    20301144 lab6 in eigth step Makefile has some errors ,this step has corrected

--- os/target/riscv64gc-unknown-none-elf/debug/deps/bit_field-b622eb5f09d3b6cb.d
+++
@@ -1,5 +0,0 @@
-d:\GardenerOS\os\target\riscv64gc-unknown-none-elf\debug\deps\bit_field-b622eb5f09d3b6cb.rmeta: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\bit_field-0.10.1\src\lib.rs
-
-d:\GardenerOS\os\target\riscv64gc-unknown-none-elf\debug\deps\bit_field-b622eb5f09d3b6cb.d: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\bit_field-0.10.1\src\lib.rs
-
-C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\bit_field-0.10.1\src\lib.rs:
