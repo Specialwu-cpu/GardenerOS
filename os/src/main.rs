4446cbd5b7b71581e4eb5b936bfa40333d101c6e
Specialwu-cpu <1307053712@qq.com>
2022-12-02 15:41:58 +0800

    20301144 lab6 first step

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 use core::arch::global_asm;
+
+extern crate alloc;
 
 #[macro_use]
 mod console;
@@ -14,6 +17,7 @@ mod loader;
 mod config;
 mod timer;
 mod task;
+mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -30,6 +34,7 @@ fn clear_bss() {
 pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
+    mm::init();
     trap::init();
     loader::load_apps();
     task::run_first_task();
