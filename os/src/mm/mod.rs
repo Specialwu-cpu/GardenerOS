5078fbec7eb1fc24013dc3a253ff60e4d73a96c8
Specialwu-cpu <1307053712@qq.com>
2022-12-02 17:27:04 +0800

    20301144 lab6 fourth step

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,10 +1,16 @@
 mod heap_allocator;
 mod page_table;
+mod address;
+mod frame_allocator;
 
 use page_table::{PTEFlags};
+pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum, StepByOne};
+pub use frame_allocator::{FrameTracker, frame_alloc};
 pub use page_table::{PageTableEntry};
 
 pub fn init() {
     heap_allocator::init_heap();
     heap_allocator::heap_test();
+    frame_allocator::init_frame_allocator();
+    frame_allocator::frame_allocator_test();
 }
---
+++ os/target/debug/deps/riscv_target-eb16865ad097cfe5.d
@@ -0,0 +1,7 @@
+d:\GardenerOS\os\target\debug\deps\riscv_target-eb16865ad097cfe5.rmeta: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\riscv-target-0.1.2\src\lib.rs
+
+d:\GardenerOS\os\target\debug\deps\libriscv_target-eb16865ad097cfe5.rlib: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\riscv-target-0.1.2\src\lib.rs
+
+d:\GardenerOS\os\target\debug\deps\riscv_target-eb16865ad097cfe5.d: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\riscv-target-0.1.2\src\lib.rs
+
+C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\riscv-target-0.1.2\src\lib.rs:
