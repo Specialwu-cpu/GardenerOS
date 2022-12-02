6963aa5f08b0e4370624c7292ede3158a68366ae
Specialwu-cpu <1307053712@qq.com>
2022-12-02 19:53:50 +0800

    20301144 lab6 seventh step

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -2,15 +2,17 @@ mod heap_allocator;
 mod page_table;
 mod address;
 mod frame_allocator;
+mod memory_set;
 
-use page_table::{PTEFlags};
+use page_table::{PageTable, PTEFlags};
+pub use frame_allocator::{FrameTracker, frame_alloc, frame_dealloc};
+pub use page_table::{PageTableEntry, translated_byte_buffer};
+pub use memory_set::{MemorySet, KERNEL_SPACE, MapPermission};
 pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum, StepByOne};
 pub use frame_allocator::{FrameTracker, frame_alloc};
-pub use page_table::{PageTableEntry};
 
 pub fn init() {
     heap_allocator::init_heap();
-    heap_allocator::heap_test();
     frame_allocator::init_frame_allocator();
-    frame_allocator::frame_allocator_test();
+    KERNEL_SPACE.exclusive_access().activate();
 }
