d6e50982d28401e0af83f16b656410fd8a0a6f99
Specialwu-cpu <1307053712@qq.com>
2022-12-02 17:52:11 +0800

    20301144 lab6 sixth step

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/zero-f2561252a7155bad.d
@@ -0,0 +1,5 @@
+d:\GardenerOS\os\target\riscv64gc-unknown-none-elf\debug\deps\zero-f2561252a7155bad.rmeta: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\zero-0.1.3\src\lib.rs
+
+d:\GardenerOS\os\target\riscv64gc-unknown-none-elf\debug\deps\zero-f2561252a7155bad.d: C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\zero-0.1.3\src\lib.rs
+
+C:\Users\DELL\.cargo\registry\src\github.com-1ecc6299db9ec823\zero-0.1.3\src\lib.rs:
